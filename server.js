e2be7cf2e712e50c53bdd36705a7634daafe27e8
Bryan Housel <bryan@mapbox.com>
2016-05-21 08:30:04 -0400

    Add server

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var http = require('http');
+var WhooTS = require('.');
+
+const PORT = 8080;
+
+function handleRequest(request, response) {
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
