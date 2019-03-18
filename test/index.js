5a7db0a734767f10fee7c65c38725a42a080fa2d
ishiland <ishiland@gmail.com>
2019-03-18 15:14:20 -0400

    Update to new WMS URL

--- test/index.js
+++ test/index.js
@@ -6,10 +6,10 @@ var WhooTS = require('../.');
 test('WhooTS', function(t) {
 
     t.test('getURL', function(t) {
-        var baseUrl = 'http://geodata.state.nj.us/imagerywms/Natural2015';
+        var baseUrl = 'https://img.nj.gov/imagerywms/Natural2015';
         var layer = 'Natural2015';
         var url = WhooTS.getURL(baseUrl, layer, 154308, 197167, 19);
-        t.deepEqual(url, 'http://geodata.state.nj.us/imagerywms/Natural2015?' +
+        t.deepEqual(url, 'https://img.nj.gov/imagerywms/Natural2015?' +
             'bbox=-8242663.382160267,4966572.349857613,-8242586.945131982,4966648.786885899' +
             '&format=image/png' +
             '&service=WMS' +
